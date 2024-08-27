f7769670ad68d30333113796cded0f4f4e111920
Angelique Rivera <angeliqueriveramorales@gmail.com>
2024-08-27 17:14:47 -0400

    feat #3: add index route in Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,11 +5,17 @@ import "../globals.css";
 import "tailwindcss/tailwind.css";
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleNav = () => {};
   return (
     <nav classname="fixed w-full h-24 shadow-xl bg-purple-500">
       <div className="justify-evenly items-center h-full w-full px-6 2xl:px-16">
         <div>
           <ul className="hidden sm:flex gap-5">
+            <Link href="/">
+              <li className="hover:border-b-purple-950 text-xl">Home</li>
+            </Link>
             <Link href="/calendar">
               <li className="hover:border-b-purple-950 text-xl">Events</li>
             </Link>
