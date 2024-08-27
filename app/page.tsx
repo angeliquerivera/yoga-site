a11ae05ba7f0cac91a8105f15a95f7c492862d96
Angelique Rivera <angeliqueriveramorales@gmail.com>
2024-08-27 15:14:29 -0400

    feat #3: add Navbar to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 import "./globals.css";
 import "tailwindcss/tailwind.css";
 
@@ -8,6 +9,7 @@ export default function Home() {
       <h1 className="text-3xl font-bold underline">
         Inner Peace and Outer Strength
       </h1>
+      <Navbar />
     </main>
   );
 }
