3ba883900739b2d0d73cfe108322d711b9f35864
Angelique Rivera <angeliqueriveramorales@gmail.com>
2024-08-27 17:01:05 -0400

    feat #3: more Navbar edits

--- app/main.tsx
+++ app/main.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Navbar from "./components/Navbar";
 import "./globals.css";
 import "tailwindcss/tailwind.css";
 
@@ -9,7 +8,6 @@ export default function Home() {
       <h1 className="text-3xl font-bold underline">
         Inner Peace and Outer Strength
       </h1>
-      <Navbar />
     </main>
   );
 }
