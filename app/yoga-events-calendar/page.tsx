8ca34289510d3ce087ca30b5b9b140b2d529fd90
Angelique Rivera <angeliqueriveramorales@gmail.com>
2024-08-27 17:09:35 -0400

    feat #3: fix syntax error in layout.tsx

--- app/yoga-events-calendar/page.tsx
+++ app/yoga-events-calendar/page.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Navbar from "./components/Navbar";
 import "../globals.css";
 import "tailwindcss/tailwind.css";
 
 export default function Calendar() {
   return (
     <main>
-      <Navbar />
       <h2>Upcoming Events</h2>
     </main>
   );
