0113b381bb29490a9b05f9547221e8bfba5dc900
Angelique Rivera <angeliqueriveramorales@gmail.com>
2024-08-27 15:40:46 -0400

    feat #3: add style classes in global.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#d7bde2",
+      },
+    },
   },
   plugins: [],
 };
