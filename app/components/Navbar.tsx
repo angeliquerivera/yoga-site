85abf414c2683a900e8aba8d385cb5a8c651191c
Angelique Rivera <angeliqueriveramorales@gmail.com>
2024-08-27 16:50:29 -0400

    feat #3: add styling to navbar and links

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,27 @@
 import React from "react";
+import { useState } from "react";
 import Link from "next/link";
 import "../globals.css";
 import "tailwindcss/tailwind.css";
 
 export default function Navbar() {
   return (
-    <nav>
-      <Link href="/calendar">Events</Link>
-      <Link href="/bio">Bio</Link>
-      <Link href="/about">About</Link>
+    <nav classname="fixed w-full h-24 shadow-xl bg-purple-500">
+      <div className="justify-evenly items-center h-full w-full px-6 2xl:px-16">
+        <div>
+          <ul className="hidden sm:flex gap-5">
+            <Link href="/calendar">
+              <li className="hover:border-b-purple-950 text-xl">Events</li>
+            </Link>
+            <Link href="/bio">
+              <li className="hover:border-b-purple-950 text-xl">Bio</li>
+            </Link>
+            <Link href="/about">
+              <li className="hover:border-b-purple-950 text-xl">About</li>
+            </Link>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
