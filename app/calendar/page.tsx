1061b682be941942622cd4d2adda65f2047aedc0
Angelique Rivera <78448875+angeliquerivera@users.noreply.github.com>
2024-09-12 15:24:42 -0400

    YOGA-4 Add images to home page (#11)
    * feat #1: fix relative path for global.css imports in pages (#9) (#10)
    
    * feat #4: add images folder with home page pics
    
    * feat: #4: add images to home page
    
    * feat #4: code cleanup
    
    * feat #4: add pics to home page
    
    * feat #4: remove unnecessary HomePage component
    
    * feat #4: fix Navbar calendar page route/redirect error
    
    * feat #4: last commit before branch switch/merge
    
    * feat #4: untrack files in .gitignore
    
    * feat #4: edit Navbar component placement in layout.tsx
    
    * feat #4: edit code in Navbar.tsx

---
+++ app/calendar/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../globals.css";
+import "tailwindcss/tailwind.css";
+
+export default function Calendar() {
+  return (
+    <main>
+      <h2>Upcoming Events</h2>
+    </main>
+  );
+}
