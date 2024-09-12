a82b4d93b8ac38a44c34eef49b659a2d97ebdcb9
Angelique Rivera <angeliqueriveramorales@gmail.com>
2024-09-12 15:54:39 -0400

    feat #7: add bio image to bio page

--- app/bio/page.tsx
+++ app/bio/page.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Image from "next/image";
+import biopic from "../public/biopic.jpg";
 import "../globals.css";
 import "tailwindcss/tailwind.css";
 
@@ -6,6 +8,22 @@ export default function Bio() {
   return (
     <main>
       <h2>Bio</h2>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <div>
+          <Image src={biopic} />
+        </div>
+        <div>
+          <p>
+            Lorem ipsum odor amet, consectetuer adipiscing elit. Amet quam sem
+            ante dui in, facilisis nisi sit non. Cursus vulputate vel donec
+            ultrices aliquet condimentum quam varius. Leo vehicula nisi
+            tincidunt class lobortis placerat curabitur maecenas. Dignissim
+            torquent tempor penatibus fermentum ultricies interdum leo auctor.
+            Libero elit interdum vitae varius; ornare laoreet fringilla
+            adipiscing.
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
