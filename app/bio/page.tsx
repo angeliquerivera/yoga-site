348b99ef1e4f8180f3bba20993f01cbfbc079deb
Angelique Rivera <angeliqueriveramorales@gmail.com>
2024-10-09 15:23:27 -0400

    edit html tags and nesting issues in bio page

--- app/bio/page.tsx
+++ app/bio/page.tsx
@@ -13,67 +13,68 @@ const montserrat = Montserrat({
 
 export default function Bio() {
   return (
-    <main>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyItems: "center",
-          gap: "16px",
-        }}
-      >
-        <div>
-          <Image
-            src={biopic}
-            alt="biopic"
-            width="1600"
-            height="1200"
-            className="rounded-full aspect-square object-cover"
-          />
-        </div>
-        <div className="flex flex-col gap-2">
-          <p className={montserrat.className}>
-            I have been a correction officer, working in NY State and county
-            prisons for 20 years. 14 years ago, desperate to find a healthy
-            outlet and relieve the stresses of my job, I turned to yoga. I had
-            always heard that yoga was good for stress. I decided to try a
-            class, and instantly fell in love. This is what my body and mind had
-            been looking for. I started with Hot Yoga, and 6 months later, tried
-            all different styles of yoga. I felt at peace and blissful every
-            time I was on my mat. I soon realized that yoga was much more than
-            physical flexibility and strength. It was also about building mental
-            flexibility and strength. I noticed a huge difference, not just in
-            my body, but in my perceptions, attitudes, and thoughts, which
-            influenced my actions. I decided that I needed to share all the
-            benefits of yoga with as many people as I could. It's been my
-            passion and my dream to open up a yoga studio. I began doing Yoga
-            Teacher trainings, to further my own practice, and to expand my
-            knowledge of the many benefits of various styles of yoga.{" "}
+    <>
+      <main>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyItems: "center",
+            gap: "16px",
+          }}
+        >
+          <div>
+            <Image
+              src={biopic}
+              alt="biopic"
+              width="1600"
+              height="1200"
+              className="rounded-full aspect-square object-cover"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className={montserrat.className}>
+              I have been a correction officer, working in NY State and county
+              prisons for 20 years. 14 years ago, desperate to find a healthy
+              outlet and relieve the stresses of my job, I turned to yoga. I had
+              always heard that yoga was good for stress. I decided to try a
+              class, and instantly fell in love. This is what my body and mind
+              had been looking for. I started with Hot Yoga, and 6 months later,
+              tried all different styles of yoga. I felt at peace and blissful
+              every time I was on my mat. I soon realized that yoga was much
+              more than physical flexibility and strength. It was also about
+              building mental flexibility and strength. I noticed a huge
+              difference, not just in my body, but in my perceptions, attitudes,
+              and thoughts, which influenced my actions. I decided that I needed
+              to share all the benefits of yoga with as many people as I could.
+              It's been my passion and my dream to open up a yoga studio. I
+              began doing Yoga Teacher trainings, to further my own practice,
+              and to expand my knowledge of the many benefits of various styles
+              of yoga.
+            </p>
             <blockquote className={montserrat.className}>
-              {" "}
-              <p className="tab">
-                My goals are to help people understand that:{" "}
-                <blockquote className="indent-28">
-                  {" "}
-                  1. EVERYONE AND ANYONE can do yoga
-                </blockquote>{" "}
-                <blockquote className="indent-28">
-                  2. The MANY benefits of yoga
-                </blockquote>
+              <p className="indent-6">
+                My goals are to help people understand:
               </p>
+              <ol>
+                <li className="indent-14">
+                  1. EVERYONE AND ANYONE can do yoga
+                </li>
+                <li className="indent-14">2. The MANY benefits of yoga</li>
+              </ol>
             </blockquote>
-          </p>
+          </div>
+          <div>
+            <Image
+              src={biopic2}
+              alt="biopic2"
+              width="1800"
+              height="1000"
+              className="rounded-full aspect-square object-cover"
+            />
+          </div>
         </div>
-        <div>
-          <Image
-            src={biopic2}
-            alt="biopic2"
-            width="1800"
-            height="1000"
-            className="rounded-full aspect-square object-cover"
-          />
-        </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
