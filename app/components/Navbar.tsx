94481a78ef2bf45f00a0da6fb6474b5f7a9fb5fa
Angelique Rivera <angeliqueriveramorales@gmail.com>
2024-10-10 16:09:37 -0400

    feat #17: add om logo on Navbar and fix link spacing

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,47 +1,70 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { usePathname } from "next/navigation";
+import omsymbol from "../public/omsymbol.jpeg";
 import "../styles.css";
 import "tailwindcss/tailwind.css";
 
 export default function Navbar() {
   const pathname = usePathname();
 
+  const [isClick, setisClick] = useState(false);
+
+  const toggleNavbar = (): void => {
+    setisClick(!isClick);
+  };
+
   return (
-    <header className="navbar md:flex w-screen h-18 shadow-xl bg-violet-400">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-xl">InnerThoughtsOuterStrength</div>
-        <div className="justify-end">
-          <ul className="hidden sm:flex gap-6">
-            <Link
-              className={`link ${pathname === "/" ? "active" : ""}`}
-              href="/"
-            >
-              <li className="hover:border-b-purple-800 text-xl ">Home</li>
-            </Link>
-            <Link
-              className={`link ${pathname === "/contact" ? "active" : ""}`}
-              href="/contact"
-            >
-              <li className="hover:border-b-purple-800 text-xl">Contact Us</li>
-            </Link>
-            <Link
-              className={`link ${pathname === "/calendar" ? "active" : ""}`}
-              href="/calendar"
-            >
-              <li className="hover:border-b-purple-800 text-xl">Calendar</li>
-            </Link>
-            <Link
-              className={`link ${pathname === "/bio" ? "active" : ""}`}
-              href="/bio"
-            >
-              <li className="hover:border-b-purple-800 text-xl">Bio</li>
-            </Link>
-          </ul>
-        </div>
-      </div>
-    </header>
+    <>
+      <header className="bg-violet-400 p-0">
+        <nav className="flex justify-between items-baseline">
+          <div>
+            <Image
+              src={omsymbol}
+              alt="omsymbol"
+              width="75"
+              height="75"
+              className="rounded-full aspect-square object-cover"
+            />
+            <span className="text-purple-800 text-lg font-bold">
+              Inner Peace Outer Strength
+            </span>
+          </div>
+          <div className="justify-between">
+            <ul className="flex-wrap flex gap-6">
+              <Link
+                className={`link ${pathname === "/" ? "active" : ""}`}
+                href="/"
+              >
+                <li className="hover:border-b-purple-800 text-xl ">Home</li>
+              </Link>
+              <Link
+                className={`link ${pathname === "/bio" ? "active" : ""}`}
+                href="/bio"
+              >
+                <li className="hover:border-b-purple-800 text-xl">Bio</li>
+              </Link>
+              <Link
+                className={`link ${pathname === "/calendar" ? "active" : ""}`}
+                href="/calendar"
+              >
+                <li className="hover:border-b-purple-800 text-xl">Calendar</li>
+              </Link>
+              <Link
+                className={`link ${pathname === "/contact" ? "active" : ""}`}
+                href="/contact"
+              >
+                <li className="hover:border-b-purple-800 text-xl">
+                  Contact Us
+                </li>
+              </Link>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
-import React from "react";
-import "./styles.css";
-import "tailwindcss/tailwind.css";
 import Image from "next/image";
 import firsthome from "./public/firsthome.jpg";
 import secondhome from "./public/secondhome.jpg";
+import React from "react";
+import "./styles.css";
+import "tailwindcss/tailwind.css";
 
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">
-        Inner Peace and Outer Strength
-      </h1>
       <div style={{ display: "flex", flexDirection: "row", gap: "15px" }}>
         <div>
           <Image src={firsthome} alt="firsthome" width="400" height="400" />
