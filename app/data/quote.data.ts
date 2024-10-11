6e068c5ac3cb55027755a2f47d5ba6b9cdae5677
Angelique Rivera <78448875+angeliquerivera@users.noreply.github.com>
2024-10-11 18:15:47 -0400

    YOGA-21 Add quotes in bio (#24)
    * add data folder with quotes data file and quote component
    
    * feat #21: add quote data
    
    * feat #21: add quote block component to bio page

---
+++ app/data/quote.data.ts
@@ -0,0 +1,13 @@
+export interface Quote {
+  text: string;
+  author: string;
+}
+
+const quote: Quote[] = [
+  {
+    text: "LOKAH SAMASTAH SUKHINO BHAVANTU: May all beings everywhere be happy and free, and may the thoughts, words, and actions of my own life contribute in some way to that happiness and to that freedom for all.",
+    author: "Mangala Matra",
+  },
+];
+
+export default quote;
