d417beff2038a49308fbbd6e2d1596a4cd510bc8
Angelique Rivera <78448875+angeliquerivera@users.noreply.github.com>
2024-11-14 00:06:34 -0500

    Yoga 30 responsive design (#45)
    * feat #30: fix left text section of home component
    
    * feat #30: add responsive design to text area and images on Home page
    
    * feat #40: fix padding in divs for Home page items

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,36 +2,37 @@ import React from "react";
 import Image from "next/image";
 import firsthome from "../public/firsthome.jpg";
 import secondhome from "../public/secondhome.jpg";
+import "../styles/styles.css";
 
 export default function Hero() {
   return (
-    <section className="bg-transparent p-5">
-      <div className="container flex flex-col md:flex-row items-center mx-auto mt-6 space-y-4 space-x-0 md:space-y-0">
-        <div className="flex flex-col mb-96 space-y-10 md:w-1/3 bg-purple-400 bg-opacity-50">
-          <h1 className="max-w-screen-lg text-4xl font-bold text-center md:text-5xl md:text-left">
+    <section className="relative bg-transparent p-8">
+      <div className="container mx-auto flex flex-col sm:flex-row items-center space-y-6 sm:space-y-0 sm:space-x-8 relative z-10 bg-purple-400 bg-opacity-50 bg-custom-background">
+        <div className="p-6 flex flex-col space-y-6 sm:w-1/3 sm:space-y-10 sm:text-left  ">
+          <h1 className="text-4xl font-bold text-center sm:text-5xl sm:text-left">
             Yoga For All
           </h1>
-          <p className="max-w-lg text-center md:text-left">
+          <p className="text-lg sm:text-xl max-w-lg mx-auto sm:mx-0">
             Begin your wellness journey
           </p>
-        </div>{" "}
-        <div className="max-w-2xl mx-auto text-center">
-          <div className="flex items-center space-x-8">
-            <Image
-              src={firsthome}
-              alt="firsthome"
-              width="350"
-              height="350"
-              className="rounded-lg object-cover"
-            />
-            <Image
-              src={secondhome}
-              alt="secondhome"
-              width="350"
-              height="350"
-              className="rounded-lg object-cover"
-            />
-          </div>
+        </div>
+        <div className="flex flex-row justify-evenly w-2/3">
+          <Image
+            src={secondhome}
+            alt="secondhome"
+            layout="responsive"
+            width={400}
+            height={400}
+            className="w-full h-auto rounded-lg shadow-lg"
+          />
+          <Image
+            src={firsthome}
+            alt="firsthome"
+            layout="responsive"
+            width={400}
+            height={400}
+            className="w-full h-auto rounded-lg shadow-lg"
+          />
         </div>
       </div>
     </section>
