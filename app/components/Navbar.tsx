e53a57bf78e1f64bbaabaab5aabb0e99080b178a
Angelique Rivera <78448875+angeliquerivera@users.noreply.github.com>
2024-11-13 19:14:01 -0500

    Yoga 14 navbar edits (#44)
    * feat #14: edit margin spacing for Navbar links
    
    * feat #14: add sticky feature to Navbar
    
    * feat #14: add hamburger icon to mobile view
    
    * feat #14: edit hamburger icon classes

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,53 +8,75 @@ import "../styles/styles.css";
 import "tailwindcss/tailwind.css";
 
 export default function Navbar() {
-  const [isClick, setisClick] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const toggleNavbar = (): void => {
-    setisClick(!isClick);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
 
   return (
     <>
-      <header className="bg-violet-400 p-0">
-        <nav className="flex justify-between place-items-baseline">
-          <div className="flex items-baseline">
-            <Image
-              src={omsymbol}
-              alt="omsymbol"
-              className="h-16 w-16 rounded-full aspect-square object-cover"
-            />
-            <span className="text-purple-800 text-2xl font-extrabold">
-              Inner Peace & Outer Strength LLC
-            </span>
-          </div>
+      <nav className="flex justify-between place-items-baseline bg-violet-400 sticky top-0 z-50 lg:static ">
+        <div className="flex items-baseline lg:flex align-text-bottom">
+          <Image
+            src={omsymbol}
+            alt="omsymbol"
+            className="h-16 w-16 rounded-full aspect-square object-cover ml-2"
+          />
+          <span className="text-purple-900 text-2xl font-extrabold">
+            Inner Peace & Outer Strength LLC
+          </span>
+        </div>
 
-          <div className="flex-col">
-            <ul className="flex-wrap flex gap-4">
-              <Link href="/">
-                <li className="hover:text-purple-800 hover:underline text-xl">
-                  Home
-                </li>
-              </Link>
-              <Link href="/bio">
-                <li className="hover:text-purple-800 hover:underline text-xl">
-                  Bio
-                </li>
-              </Link>
-              <Link href="/calendar">
-                <li className="hover:text-purple-800 hover:underline text-xl">
-                  Calendar
-                </li>
-              </Link>
-              <Link href="/contact">
-                <li className="hover:text-purple-800 hover:underline text-xl">
-                  Contact Us
-                </li>
-              </Link>
-            </ul>
-          </div>
-        </nav>
-      </header>
+        <button
+          className="lg:hidden text-purple-900 focus:outline-none"
+          onClick={toggleMenu}
+        >
+          <svg
+            className="w-10 h-10"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+
+        <div
+          className={`${
+            isMenuOpen ? "block" : "hidden"
+          } lg:flex flex-col lg:flex-row lg:space-x-6 space-y-4 lg:space-y-0 mr-4`}
+        >
+          <ul className="flex-wrap sm:flex gap-4">
+            <Link href="/">
+              <li className="hover:text-purple-800 hover:underline text-xl">
+                Home
+              </li>
+            </Link>
+            <Link href="/bio">
+              <li className="hover:text-purple-800 hover:underline text-xl">
+                Bio
+              </li>
+            </Link>
+            <Link href="/calendar">
+              <li className="hover:text-purple-800 hover:underline text-xl">
+                Calendar
+              </li>
+            </Link>
+            <Link href="/contact">
+              <li className="hover:text-purple-800 hover:underline text-xl">
+                Contact Us
+              </li>
+            </Link>
+          </ul>
+        </div>
+      </nav>
     </>
   );
 }
