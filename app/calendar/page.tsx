55040a638812fb3494c2335ec5c645155fcc311d
Angelique Rivera <angeliqueriveramorales@gmail.com>
2025-01-06 20:03:45 -0500

    remove events from main

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -7,36 +7,7 @@ import "tailwindcss/tailwind.css";
 export const metadata: Metadata = {
   title: "Calendar",
 };
-const eventsData = [
-  {
-    id: 1,
-    title: "Sync, Seat, and Be Merry!",
-    date: "12-28-2024",
-    description:
-      "Celebrate Christmas with a 1 hour Chair Yoga class, followed by snacks and beverages",
-    location: "12 Sunrise Hill Road, Fishkill, NY",
-    googleMapsUrl: "https://maps.app.goo.gl/DX61euWwEaR4Hnh76",
-    time: "2:00pm-4:00pm",
-    price: "$15.00 per person",
-    rsvpDeadline: "Dec 22nd",
-    googleCalendarUrl:
-      "https://www.google.com/calendar/render?action=TEMPLATE&text=Sync%2C+Seat%2C+and+Be+Merry%21&dates=20241228T190000Z%2F20241228T210000Z&details=Celebrate+Christmas+with+a+1+hour+Chair+Yoga+class%2C+followed+by+snacks+and+beverages&location=12+Sunrise+Hill+Road%2C+Fishkill%2C+NY",
-  },
-  {
-    id: 2,
-    title: "New Year, New Outlook",
-    date: "01-10-2025",
-    description:
-      "Celebrate the New Year with breathwork followed by a 45 minute invigorating Yinyasa Yoga class and a 15 minute Savasana. Light snacks and beverages after.",
-    location: "Residence Inn White Plains",
-    googleMapsUrl: "https://maps.app.goo.gl/DX61euWwEaR4Hnh76",
-    time: "7:00pm-9:30pm",
-    price: "$25.00 per person",
-    rsvpDeadline: "Jan 5th",
-    googleCalendarUrl:
-      "https://www.google.com/calendar/render?action=TEMPLATE&text=New+Year%2C+New+Outlook&dates=20250110T190000Z%2F20250110T213000Z&details=Celebrate+the+New+Year+with+breathwork+followed+by+a+45+minute+invigorating+Yinyasa+Yoga+class+and+a+15+minute.+Light+snacks+and+beverages+after.&location=Residence+Inn+White+Plains",
-  },
-];
+const eventsData = [];
 
 export default function Workshops() {
   return (
@@ -45,9 +16,7 @@ export default function Workshops() {
         <EventList events={eventsData} />
       </div>
       <div className="flex justify-center">
-        <p className="font-bold text-lg">
-          **Upcoming February events will be posted on December 12th**
-        </p>
+        <p className="font-bold text-lg">**Upcoming February events TBA**</p>
       </div>
       <div className="flex justify-center">
         <p className="font-bold text-lg">
