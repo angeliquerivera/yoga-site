a8c4a23cb4b8174765489f82693bfba6883ef224
Angelique Rivera <angeliqueriveramorales@gmail.com>
2025-06-16 22:36:16 -0400

    feat(#161): remove everything tina related

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '83e03a32d52362feb6e9c2e06b334aa7662ea4ee', queries,  });
-export default client;
-  
